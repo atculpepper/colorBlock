e0169585433e11090fee8f1227a8884e7af6f301
Anne Culpepper <anne.culpepper@gmail.com>
2020-03-12 10:35:21 -0500

    stage 1 complete

---
+++ colorBlock.js
@@ -0,0 +1,24 @@
+$(document).ready(init);
+redReady();
+
+let numRedClicks = 0;
+
+let numBlueClicks = 0;
+
+let numGreenClicks = 0;
+
+let numYellowClicks = 0;
+
+function redReady() {
+  //target redNumber by class
+  //display numRedClicks
+  let redEl = $(".redNumber");
+  redEl.empty();
+  redEl.append(redNumClicks);
+}
+
+function blueReady() {}
+
+function greenReady() {}
+
+function yellowReady() {}
